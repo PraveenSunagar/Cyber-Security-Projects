0ab175bab283dfe930cb9154c0e5ff9db93fc8a4
praveen p sunagar <Praveensunagar39@gmail.com>
2026-09-21 22:00:41 +0530

    Create SecurityEvent model in backend schemas
    Define SecurityEvent model with various attributes.

--- Hard/# backend-schemas.py
+++ Hard/# backend-schemas.py
@@ -1,1 +1,19 @@
+# backend/schemas.py
 
+from pydantic import BaseModel
+
+
+class SecurityEvent(BaseModel):
+
+    source_ip: str
+    destination_ip: str | None = None
+
+    event_type: str
+
+    failed_logins: int = 0
+    unique_ports: int = 0
+    dns_requests: int = 0
+
+    username: str | None = None
+
+    timestamp: str | None = None
